f8e1c33079b9a6c3d0a4edd361ec47019ba53a78
LLipter <katsudonn@163.com>
2019-01-31 00:42:05 -0800

    my solution

--- disc01/filter.cpp
+++ disc01/filter.cpp
@@ -11,14 +11,27 @@ Filter::Filter(const std::string& filename) {
 }
 
 void Filter::read(std::string filename) {
-	// FIXME
+	std::ifstream ifs(filename);
+	ifs >> width >> height;
+	float f;
+	kernel.clear();
+	for(int i=0;i<width*height;i++){
+		ifs >> f;
+		kernel.push_back(f);
+	}
+	ifs.close();
+	normalize();
 }
 
 void Filter::normalize() {
-	// FIXME
+	float sum = 0;
+	for(int i=0;i<width*height;i++)
+		sum += kernel[i];
+	for(int i=0;i<width*height;i++)
+		kernel[i] /= sum;
 }
 
 const float& Filter::at(int x, int y) const {
-	// FIXME
+	return kernel[y * width + x];
 }
 
--- disc01/image.cpp
+++ disc01/image.cpp
@@ -16,17 +16,42 @@ Image::Image(const unsigned& w, const unsigned& h)
 }
 
 void Image::read(std::string filename) {
-	// FIXME
+	//decode
+	unsigned error = lodepng::decode(data, width, height, filename);
+	//if there's an error, display it
+	if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+	//the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
 }
 
 void Image::write(std::string filename) const {
-	// FIXME
+	//Encode the image
+	unsigned error = lodepng::encode(filename, data, width, height);
+	//if there's an error, display it
+	if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
 }
 
 uint8_t* Image::at(int x, int y) {
-	// FIXME
+	return &data[(y * width + x) * 4];
 }
 
 Image Image::operator*(const Filter& filter) {
-	// FIXME
+	Image result(width, height);
+	for(int c=0;c<4;c++){
+		for(int x=0;x<width;  x++){
+			for(int y=0;y<height;y++){
+                float newValue = 0;
+				for(int i=0;i<filter.width;i++){
+					for(int j=0;j<filter.height;j++){
+						int x_pos = x - filter.width/2 + i;
+						int y_pos = y - filter.height/2 + j;
+						if(x_pos < 0 || x_pos >= width || y_pos < 0 || y_pos >= height)
+                            continue;
+						newValue += at(x_pos, y_pos)[c] * filter.at(i, j);
+					}
+				}
+				*(result.at(x, y) + c) = (uint8_t)newValue;
+			}
+		}
+	}
+	return result;
 }
